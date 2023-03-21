14aa8ab23e9bb0dc0d1e951de4e8f0deb78d9e34
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-03-21 19:07:34 +0000

    added delete_all() to album and artist

--- album_manager/console.py
+++ album_manager/console.py
@@ -6,12 +6,18 @@ import repositories.artist_repo as artist_repo
 
 # I want to add artists to the database. 
 
+artist_repo.delete_all()
+album_repo.delete_all()
+
 artist1 = Artist("Bob Marley")
 artist_repo.save(artist1)
 
 artist2 = Artist("Harry Styles")
 artist_repo.save(artist2)
 
+
+album1 = Album("Boo", "Horror Rock", "Ghost")
+album_repo.save(album1)
 # this result is a list that was define in artist_repo select_all()
 result = artist_repo.select_all()
 
--- album_manager/repositories/album_repo.py
+++ album_manager/repositories/album_repo.py
@@ -1,1 +1,17 @@
 from db.run_sql import run_sql
+from models.album import Album
+
+# I want to create an album 
+
+def save(album):
+    sql = "INSERT INTO albums (title, genre, artist) VALUES (%s,%s,%s) RETURNING *"
+    values = [album.title, album.genre, album.artist]
+    results = run_sql(sql, values)
+
+    id = results[0]['id']
+    album.id = id
+    return album 
+
+def delete_all():
+    sql = "DELETE from albums"
+    run_sql(sql)
